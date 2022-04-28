40ccfcf88d4d3182375ee990e4790d5990480e8c
victorke19 <70530332+victorke19@users.noreply.github.com>
2022-04-28 18:48:17 -0400

    used my brain and runs makes a lot more sense, should run smoother

--- test/test_runs.py
+++ test/test_runs.py
@@ -1,9 +1,11 @@
 from pickle import FALSE
+from click import prompt
 from pymysql import DATETIME
 
 import pytest
 import json
 import datetime
+import time
 
 
 # PROMPTS = [
@@ -19,73 +21,78 @@ import datetime
 
 def test_create_run(client, cursor, user, session):
   assert 1 == cursor.execute('INSERT INTO sprint_prompts (start, end) VALUES ("A", "B")')
+  cursor.execute('SELECT prompt_id FROM sprint_prompts')
+  prompt_id = cursor.fetchone().get('prompt_id')
   resp = client.post("/api/runs", json={
-    "prompt_id": 8
+    "prompt_id": prompt_id
   })
   assert resp.status_code == 200
-
-  assert 1 == cursor.execute("DELETE FROM sprint_runs WHERE prompt_id=%s", (8,))
+  assert 1 == cursor.execute("DELETE FROM sprint_runs WHERE prompt_id=%s", (prompt_id,))
   assert 1 == cursor.execute("DELETE FROM sprint_prompts")
 
-#needs work
 def test_finish_run(client, cursor, user, session):
   assert 1 == cursor.execute('INSERT INTO sprint_prompts (start, end) VALUES ("A", "B")')
-  assert 1 == cursor.execute('INSERT INTO sprint_runs (prompt_id) VALUES (9)')
+  cursor.execute('SELECT prompt_id FROM sprint_prompts')
+  prompt_id = cursor.fetchone().get('prompt_id')
+  assert 1 == cursor.execute('INSERT INTO sprint_runs (prompt_id) VALUES (%s)', (prompt_id))
   cursor.execute('SELECT run_id FROM sprint_runs')
-  print(cursor.fetchall())
-  resp = client.post("/api/runs/2", json={
-    "run_id": 2,
-    "start_time": 1,
-    "end_time": 100,
+  run_id = cursor.fetchone().get('run_id')
+  resp = client.patch("/api/runs/" + str(run_id), json={
+    "run_id": run_id,
+    "start_time": time.time(),
+    "end_time": time.time(),
     "path": json.dumps([20, 21])
   })
-  #assert resp.status_code == 200
-  #print(resp.json)
+  assert resp.status_code == 200
   cursor.execute("SELECT run_id FROM sprint_runs")
-  assert cursor.fetchall()[0] == {'run_id': 2}
-  # cursor.execute("SELECT path FROM sprint_runs WHERE run_id = 2")
-  # assert cursor.fetchall()[0] == {'path': [20, 21]}
-  assert 1 == cursor.execute("DELETE FROM sprint_runs WHERE prompt_id=%s", (9,))
+  assert cursor.fetchone().get('run_id') == run_id
+  cursor.execute("SELECT path FROM sprint_runs WHERE run_id = %s", (run_id,))
+  assert cursor.fetchone().get('path') == '"[20, 21]"'
+  assert 1 == cursor.execute("DELETE FROM sprint_runs WHERE prompt_id=%s", (prompt_id,))
   assert 1 == cursor.execute("DELETE FROM sprint_prompts")
-
-
 
 def test_update_anonymous_sprint_run(client, cursor):
   assert 1 == cursor.execute('INSERT INTO sprint_prompts (start, end) VALUES ("A", "B")')
-  assert 1 == cursor.execute('INSERT INTO sprint_runs (prompt_id) VALUES (10)')
+  cursor.execute('SELECT prompt_id FROM sprint_prompts')
+  prompt_id = cursor.fetchone().get('prompt_id')
+  assert 1 == cursor.execute('INSERT INTO sprint_runs (prompt_id) VALUES (%s)', (prompt_id,))
   cursor.execute('SELECT run_id FROM sprint_runs')
-  print(cursor.fetchall())
+  run_id = cursor.fetchone().get('run_id')
   resp = client.post("/api/runs/update_anonymous", json={
+    # feeling iffy about hard coding this but dont know how to get valid user wo having to add one or import into session which will mess this test up
     "user_id": 14
   })
-  cursor.execute("SELECT user_id FROM sprint_runs WHERE run_id = 3")
-
-  assert 1 == cursor.execute("DELETE FROM sprint_runs WHERE prompt_id=%s", (10,))
+  cursor.execute("SELECT user_id FROM sprint_runs WHERE run_id = %s", (run_id))
+  assert 1 == cursor.execute("DELETE FROM sprint_runs WHERE prompt_id=%s", (prompt_id,))
   assert 1 == cursor.execute("DELETE FROM sprint_prompts")
 
-# not returning any runs
 def test_get_all_runs(client, cursor, user, session):
   # create prompts and run
   assert 1 == cursor.execute('INSERT INTO sprint_prompts (start, end) VALUES ("A", "B")')
-  assert 1 == cursor.execute('INSERT INTO sprint_runs (prompt_id) VALUES (11)')
+  cursor.execute('SELECT prompt_id FROM sprint_prompts')
+  prompt_id = cursor.fetchone().get('prompt_id')
+  assert 1 == cursor.execute('INSERT INTO sprint_runs (prompt_id) VALUES (%s)', (prompt_id,))
+  cursor.execute('SELECT run_id FROM sprint_runs')
+  run_id = cursor.fetchone().get('run_id')
   resp = client.get('/api/runs')
   assert len(resp.json) > 0 
-  assert 1 == cursor.execute("DELETE FROM sprint_runs WHERE prompt_id=%s", (11,))
+  assert 1 == cursor.execute("DELETE FROM sprint_runs WHERE prompt_id=%s", (prompt_id,))
   assert 1 == cursor.execute("DELETE FROM sprint_prompts")
 
 def test_get_run(client, cursor, user, session): 
   assert 1 == cursor.execute('INSERT INTO sprint_prompts (start, end) VALUES ("A", "B")')
+  cursor.execute('SELECT prompt_id FROM sprint_prompts')
+  prompt_id = cursor.fetchone().get('prompt_id')
   path = json.dumps([20, 21])
-  assert 1 == cursor.execute('INSERT INTO sprint_runs (prompt_id, path, user_id) VALUES (12, %s, 25)', (path,))
-  cursor.execute('SELECT * FROM sprint_runs')
-  print(cursor.fetchall())
-  resp = client.get('/api/runs/5')
+  assert 1 == cursor.execute('INSERT INTO sprint_runs (prompt_id, path, user_id) VALUES (%s, %s, %s)', (prompt_id, path, user['user_id']))
+  cursor.execute('SELECT run_id FROM sprint_runs')
+  run_id = cursor.fetchone().get('run_id')
+  resp = client.get('/api/runs/' + str(run_id))
   assert resp.status_code == 200
-  print(resp.json) 
-  assert 1 == cursor.execute("DELETE FROM sprint_runs WHERE prompt_id=%s", (12,))
+  assert 1 == cursor.execute("DELETE FROM sprint_runs WHERE prompt_id=%s", (prompt_id,))
   assert 1 == cursor.execute("DELETE FROM sprint_prompts")
-  assert resp.json["user_id"] == 25
-  assert resp.json["run_id"] == 5
+  assert resp.json["user_id"] == user['user_id']
+  assert resp.json["run_id"] == run_id
   assert resp.json["path"] == [20, 21]
 
   # delete
