cfd2b61ed65b9097951ca7560e1c3a9fc8e5d61b
victorke19 <70530332+victorke19@users.noreply.github.com>
2022-04-29 13:50:34 -0400

    added comments to tets run and frontend

--- test/test_frontend.py
+++ test/test_frontend.py
@@ -16,22 +16,29 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
 # Update path with retrieving from online instead?
-PATH = "/Users/sean-murray/Desktop/wikipedia-speedruns/env/lib/python3.8/site-packages/seleniumbase/drivers/chromedriver"
+# PATH = "/Users/sean-murray/Desktop/wikipedia-speedruns/env/lib/python3.8/site-packages/seleniumbase/drivers/chromedriver"
 
 # Game does not allow you to play prompt of the day if you are not logged in
 def test_prompt_of_day_no_login():
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
     WebDriverWait(driver, 10)
+    # open page
     driver.get("https://wikispeedruns.com/")
     driver.maximize_window()
-    driver.implicitly_wait(20)    
+    driver.implicitly_wait(20)   
+
+    #start run
     play_button = driver.find_element(By.XPATH, "/html/body/div/div/div/div[1]/div[1]/div/div[1]/table/tbody/tr/td[1]/a")
     play_button.click()
     driver.implicitly_wait(20)
+
+    # assert that prompt of day doesnt allow you to play if youre not logged in
     alert = driver.switch_to.alert
     assert "You must be logged in to play this rated prompt" in alert.text
     alert.accept()
     driver.implicitly_wait(20)
+
+    # check if reroutes you back to home page
     assert driver.current_url == 'https://wikispeedruns.com/'
     driver.quit()
 
@@ -42,19 +49,27 @@ def test_login():
     driver.get("https://wikispeedruns.com/")
     driver.maximize_window()
     driver.implicitly_wait(20)
+
+    # click login button to go to login page
     login = driver.find_element(By.LINK_TEXT, "Login")
     login.click()
     driver.implicitly_wait(20)
+
+    # ensure on the right page
     assert driver.current_url == 'https://wikispeedruns.com/login'
     usrnm = driver.find_element(By.ID, "login")
     pswd = driver.find_element(By.ID, "password")
     usrnm.send_keys('testcase')
     pswd.send_keys('password')
+
+    # login
     driver.find_element(By.XPATH, "/html/body/div/div/div/form/button").click()
     driver.implicitly_wait(20)
     WebDriverWait(driver, 5).until(
         lambda driver: driver.current_url == "https://wikispeedruns.com/")
+    # check to see back on homepage
     assert driver.current_url == 'https://wikispeedruns.com/'
+    # check to see if logged in
     assert driver.find_element(By.XPATH, "/html/body/nav/div/div/ul/li[1]/a").text == 'testcase'
     driver.quit()
 
@@ -86,7 +101,7 @@ def test_play_after_login():
     driver.find_element(By.ID, "start-btn").click()
     driver.implicitly_wait(30)
     WebDriverWait(driver, 30)
-
+    # checking if able to play by seeing if the game is in rated mode or in practice mode, either is valid
     try:
         WebDriverWait(driver, 30)
         mode = driver.find_element(By.CLASS_NAME, "text-danger")
@@ -108,36 +123,46 @@ def test_cmmd_f():
     driver.find_element(By.ID, "start-btn").click()
     driver.implicitly_wait(20)
     WebDriverWait(driver, 10)
+    # ensure we are on wiki page so pressing command find will trigger alert
     assert driver.find_element(By.XPATH, "/html/body/div/div/div/div[4]/div[1]/div[1]/h1/i").text == 'Chickering'
+    # press command find
     ActionChains(driver).key_down(Keys.COMMAND).send_keys('F').key_up(Keys.COMMAND).perform()
     driver.implicitly_wait(20)
+    # check if alert popped up and content
     alert = driver.switch_to.alert
     assert "WARNING: Attempt to Find in page. This will be recorded." in alert.text
     alert.accept()
     driver.implicitly_wait(20)
+    # ensure we stay on same page, should not reset run
     assert driver.current_url == 'https://wikispeedruns.com/play/76'
     driver.quit()
 
 # pressing back will reset run
 def test_back_ends_run():
     opt = webdriver.ChromeOptions().add_argument("--disable-notifications")
-    #driver = webdriver.Chrome(PATH, options=opt)
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=opt)
+    # open a game
     driver.get("https://wikispeedruns.com/play/76")
     driver.maximize_window()
     driver.implicitly_wait(40)
     driver.find_element(By.ID, "start-btn").click()
     driver.implicitly_wait(20)
+    # check to make sure game has been started
     assert driver.find_element(By.XPATH, "/html/body/div/div/div/div[4]/div[1]/div[1]/h1/i").text == 'Chickering'
+    # test a couple of links so we can later test back button isnt going just back one page
     driver.find_element(By.LINK_TEXT, "Chickering, Suffolk").click()
     driver.implicitly_wait(20)
     driver.find_element(By.LINK_TEXT, "United Kingdom").click()
     driver.implicitly_wait(40)
+    # go back
     try:
         driver.back()
     except UnexpectedAlertPresentException:
+        # sometimes chrome gives "are you sure you want to leave this page, changes wont be saved" alert
+        # trying to accept that alert and move on
         driver.switch_to_window()
         WebDriverWait(driver, 15).until(ActionChains(driver).send_keys(Keys.RETURN).perform())
+        # assert back button ends run
         WebDriverWait(driver, 30).until(
             lambda driver: driver.current_url == "https://wikispeedruns.com/")
         assert driver.current_url == 'https://wikispeedruns.com/'
@@ -163,8 +188,10 @@ def test_refresh_holds_state():
     WebDriverWait(driver, 5).until(
         lambda driver: driver.current_url == "https://wikispeedruns.com/")
     driver.implicitly_wait(40)
+    # refresh page
     driver.refresh()
     driver.implicitly_wait(20)
+    # check if still logged in
     assert driver.find_element(By.XPATH, "/html/body/nav/div/div/ul/li[1]/a").text == 'testcase'
     driver.quit()
 
@@ -217,10 +244,8 @@ def test_hover_article():
     driver.implicitly_wait(40)
     driver.find_element(By.ID, "start-btn").click()
     driver.implicitly_wait(20)
-    txt = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.LINK_TEXT, "Arthur M. Chickering"))
-        )
+    txt = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.LINK_TEXT, "Arthur M. Chickering")))
     ActionChains(driver).move_to_element(txt).perform()
-    popup = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.CLASS_NAME, "tooltip-container"))
-        )
+    popup = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.CLASS_NAME, "tooltip-container")))
     assert popup.text == "Arthur Merton Chickering was a U.S. arachnologist."
     driver.quit()
--- test/test_runs.py
+++ test/test_runs.py
@@ -7,31 +7,23 @@ import json
 import datetime
 import time
 
-
-# PROMPTS = [
-#     {
-#         "start" : "Johns Hopkins University",
-#         "end" : "Baltimore", 
-#     },
-#     {
-#         "start" : "A",
-#         "end" : "B", 
-#     },
-# ]
-
-# can prob code a @BeforeEach type to make prompts etc and then cleanup function??
-
+# test to see if we can create a run
 def test_create_run(client, cursor, user, session):
+  # ensure that we add a prompt to db, run is dependent on prompt
   assert 1 == cursor.execute('INSERT INTO sprint_prompts (start, end) VALUES ("A", "B")')
   cursor.execute('SELECT prompt_id FROM sprint_prompts')
   prompt_id = cursor.fetchone().get('prompt_id')
+  # test endpoint to create run
   resp = client.post("/api/runs", json={
     "prompt_id": prompt_id
   })
+  # assert success
   assert resp.status_code == 200
+  # delete run and prompt
   assert 1 == cursor.execute("DELETE FROM sprint_runs WHERE prompt_id=%s", (prompt_id,))
   assert 1 == cursor.execute("DELETE FROM sprint_prompts")
 
+# test for an uncompleted run, to see if we can update it once finished, indicated by adding a path, start time, and end time
 def test_finish_run(client, cursor, user, session):
   assert 1 == cursor.execute('INSERT INTO sprint_prompts (start, end) VALUES ("A", "B")')
   cursor.execute('SELECT prompt_id FROM sprint_prompts')
@@ -39,21 +31,25 @@ def test_finish_run(client, cursor, user, session):
   assert 1 == cursor.execute('INSERT INTO sprint_runs (prompt_id) VALUES (%s)', (prompt_id))
   cursor.execute('SELECT run_id FROM sprint_runs')
   run_id = cursor.fetchone().get('run_id')
+  # update run with completed values
   resp = client.patch("/api/runs/" + str(run_id), json={
     "run_id": run_id,
     "start_time": time.time(),
     "end_time": time.time(),
     "path": json.dumps([20, 21])
   })
+  # ensure success
   assert resp.status_code == 200
   cursor.execute("SELECT run_id FROM sprint_runs")
   assert cursor.fetchone().get('run_id') == run_id
+  # validate run is present and that path was added to run to make it complete
   cursor.execute("SELECT path FROM sprint_runs WHERE run_id = %s", (run_id,))
   assert cursor.fetchone().get('path') == '"[20, 21]"'
+  # delete run and prompt
   assert 1 == cursor.execute("DELETE FROM sprint_runs WHERE prompt_id=%s", (prompt_id,))
   assert 1 == cursor.execute("DELETE FROM sprint_prompts")
 
-#not testing anything rn
+# testing for when run is started without user, able to add user to run
 def test_update_anonymous_sprint_run(client, cursor):
   assert 1 == cursor.execute('INSERT INTO sprint_prompts (start, end) VALUES ("A", "B")')
   cursor.execute('SELECT prompt_id FROM sprint_prompts')
@@ -69,6 +65,7 @@ def test_update_anonymous_sprint_run(client, cursor):
   assert 1 == cursor.execute("DELETE FROM sprint_runs WHERE prompt_id=%s", (prompt_id,))
   assert 1 == cursor.execute("DELETE FROM sprint_prompts")
 
+# test to see if we can grab all runs in db
 def test_get_all_runs(client, cursor, user, session):
   # create prompts and run
   assert 1 == cursor.execute('INSERT INTO sprint_prompts (start, end) VALUES ("A", "B")')
@@ -78,10 +75,12 @@ def test_get_all_runs(client, cursor, user, session):
   cursor.execute('SELECT run_id FROM sprint_runs')
   run_id = cursor.fetchone().get('run_id')
   resp = client.get('/api/runs')
+  # ensure that at least one run is present
   assert len(resp.json) > 0 
   assert 1 == cursor.execute("DELETE FROM sprint_runs WHERE prompt_id=%s", (prompt_id,))
   assert 1 == cursor.execute("DELETE FROM sprint_prompts")
 
+# testing to see if we can get a specifc run based off its run_id
 def test_get_run(client, cursor, user, session): 
   assert 1 == cursor.execute('INSERT INTO sprint_prompts (start, end) VALUES ("A", "B")')
   cursor.execute('SELECT prompt_id FROM sprint_prompts')
@@ -97,7 +96,3 @@ def test_get_run(client, cursor, user, session):
   assert resp.json["user_id"] == user['user_id']
   assert resp.json["run_id"] == run_id
   assert resp.json["path"] == [20, 21]
-
-  # delete
-  # cursor.execute('SELECT run_id FROM sprint_runs')
-  # print(cursor.fetchall())
